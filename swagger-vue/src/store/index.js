2a2c55703e0a72f4be5d470fcb485c56bf78fa4c
Levin <1837307557@qq.com>
2018-06-15 10:14:11 +0800

    添加上下文路径

--- swagger-vue/src/store/index.js
+++ swagger-vue/src/store/index.js
@@ -1,16 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VueResource from 'vue-resource'
+import Vue from "vue";
+import Vuex from "vuex";
+import VueResource from "vue-resource";
 
 Vue.use(Vuex);
 Vue.use(VueResource);
-let curWwwPath=window.document.location.href;
-let pathName=window.document.location.pathname;
-let pos=curWwwPath.indexOf(pathName);
-//获取主机地址，如： http://localhost:8083
-let localhostPaht=curWwwPath.substring(0,pos);
-const SWAGGER_URL= process.env.SWAGGER_URL===""?localhostPaht:process.env.SWAGGER_URL;
-console.log(SWAGGER_URL,process.env.SWAGGER_URL)
+
+//js获取项目根路径，如： http://localhost:8080/battcn
+function rootPath() {
+  //获取当前网址，如： http://localhost:8083/battcn/swagger-ui.html
+  const curWwwPath = window.document.location.href;
+  //获取主机地址之后的目录，如： battcn/swagger-ui.html
+  const pathName = window.document.location.pathname;
+  const pos = curWwwPath.indexOf(pathName);
+  //获取主机地址，如： http://localhost:8083
+  const localhostPaht = curWwwPath.substring(0, pos);
+  //获取带"/"的项目名，如：/battcn
+  const projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+  return (localhostPaht + projectName) + "/";
+}
+
+const SWAGGER_URL = process.env.SWAGGER_URL === "" ? rootPath() : process.env.SWAGGER_URL;
+console.log(SWAGGER_URL, process.env.SWAGGER_URL)
 /* 下拉框数据 */
 const dropDown = {
   state: {data: [], count: 0},
@@ -27,7 +37,7 @@ const leftDropDownBoxContent = {
 };
 /* 初始化:获取单选框数据， */
 function init() {
-  Vue.http.get(process.env.SWAGGER_URL + "swagger-resources").then((response) => {
+  Vue.http.get(SWAGGER_URL + "swagger-resources").then((response) => {
     dropDown.state.data = response.body;
     return true;
   }, (response) => {
@@ -36,7 +46,7 @@ function init() {
   }).then((a) => {
     if (a && dropDown.state.data[dropDown.state.count] && dropDown.state.data[dropDown.state.count] && dropDown.state.data[dropDown.state.count] && dropDown.state.data[dropDown.state.count].location) {
       /* dropDown.state.data[0]控制当前是第几个接口 */
-      Vue.http.get(process.env.SWAGGER_URL + dropDown.state.data[dropDown.state.count].location).then((response) => {
+      Vue.http.get(SWAGGER_URL + dropDown.state.data[dropDown.state.count].location).then((response) => {
         leftDropDownBoxContent.state.data = response.body;
         return true;
       }, (response) => {
