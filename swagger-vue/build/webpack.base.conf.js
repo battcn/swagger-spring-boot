e2906436a24939bfe1404f77ee90d76a98cfe5bd
_Rock <995269937@qq.com>
2018-07-13 17:39:37 +0800

    图片文件打包配置

--- swagger-vue/build/webpack.base.conf.js
+++ swagger-vue/build/webpack.base.conf.js
@@ -46,7 +46,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[ext]')
         }
       },
       {
