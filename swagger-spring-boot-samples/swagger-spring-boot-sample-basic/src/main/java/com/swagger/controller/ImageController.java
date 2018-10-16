b40ba4b6b0a68358836566254119de7269580d68
Levin <1837307557@qq.com>
2018-10-16 15:00:00 +0800

    添加图片测试接口

--- swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/ImageController.java
+++ swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/ImageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
 
 /**
@@ -23,15 +22,30 @@ import java.io.IOException;
 public class ImageController {
 
 
+    @GetMapping("/none")
+    @ApiOperation(value = "验证码（NONE）")
+    public void genCaptcha1(HttpServletResponse response) throws IOException {
+        response.setContentType("image/jpeg");
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        ServletOutputStream outputStream = response.getOutputStream();
+        final String code = NewVerifyCodeUtils.outputNumberVerifyImage(outputStream);
+        System.out.println(code);
+        outputStream.flush();
+        outputStream.close();
+    }
+
     @GetMapping
-    @ApiOperation(value = "验证码（NONE）",produces = MediaType.IMAGE_JPEG_VALUE)
+    @ApiOperation(value = "验证码（NONE）", produces = MediaType.IMAGE_JPEG_VALUE)
     public void genCaptcha(HttpServletResponse response) throws IOException {
         response.setContentType("image/jpeg");
         response.setHeader("Pragma", "No-cache");
         response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0);
         ServletOutputStream outputStream = response.getOutputStream();
-        NewVerifyCodeUtils.outputNumberVerifyImage(outputStream);
+        final String code = NewVerifyCodeUtils.outputNumberVerifyImage(outputStream);
+        System.out.println(code);
         outputStream.flush();
         outputStream.close();
     }
