5803b373f3726dc0bb6302dee17a09b512e8d534
Levin <1837307557@qq.com>
2018-10-30 14:44:33 +0800

    解决测试时候跨域问题

--- swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/BasicApplication.java
+++ swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/BasicApplication.java
@@ -2,6 +2,7 @@ package com.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,19 +19,28 @@ public class BasicApplication {
     }
 
     private static final String PATH = "/**";
+
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 设置你要允许的网站域名，如果全允许则设为 *
+        corsConfiguration.addAllowedOrigin("http://localhost:8081");
         corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        corsConfiguration.setAllowCredentials(true);
         return corsConfiguration;
     }
 
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean filterRegistrationBean() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration(PATH, buildConfig());
-        return new CorsFilter(source);
+        source.registerCorsConfiguration("/*", buildConfig());
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        // 这个顺序很重要哦，为避免麻烦请设置在最前
+        bean.setOrder(0);
+        return bean;
+        //return new CorsFilter(source);
     }
 
 }
