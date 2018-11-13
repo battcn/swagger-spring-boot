c51848a19578b1dbdfa3694ad12fc4b518089ce0
1837307557@qq.com <xiongfei>
2018-11-13 21:53:15 +0800

    提交测试案例

--- swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/JsonController.java
+++ swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/JsonController.java
@@ -3,10 +3,7 @@ package com.swagger.controller;
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * swagger
@@ -19,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "1.7", description = "阿里JSON", value = "阿里JSON")
 public class JsonController {
 
+    @GetMapping
+    public String string(String value) {
+        return value;
+    }
     @PostMapping
     public JSONObject jsonObject(@ApiParam("这是一串JSON内容")@RequestBody JSONObject value) {
         return value;
