263799811503b7ea07917d8ffbb133ae5e086b3a
Levin <1837307557@qq.com>
2018-12-06 17:19:30 +0800

    添加测试案例s

--- swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/ApiResponseController.java
+++ swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/controller/ApiResponseController.java
@@ -1,10 +1,21 @@
 package com.swagger.controller;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.swagger.entity.Address;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import springfox.documentation.spring.web.DocumentationCache;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Levin
@@ -12,18 +23,30 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping(value = "/api_responses")
-@Api(tags = "3.0.1", value = "注解测试", description = "注解测试")
+@Api(tags = {"3.0.1", "2.0.5"}, value = "注解测试11", description = "注解测试22")
 public class ApiResponseController {
 
+    @Autowired
+    private DocumentationCache documentationCache;
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     @ApiResponses({
             @ApiResponse(code = 200, message = "成功"),
             @ApiResponse(code = 204, message = "无数据")
     })
     @GetMapping("/test1")
-    public String test1() {
-
-        return "success";
+    public Set<Map<String, String>> test1() {
+        Set<Map<String, String>> classes = Sets.newLinkedHashSet();
+        Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> m : map.entrySet()) {
+            Map<String, String> hashMap = Maps.newHashMap();
+            HandlerMethod method = m.getValue();
+            final Class<?> declaringClass = method.getMethod().getDeclaringClass();
+            hashMap.put("className", declaringClass.getSimpleName());
+            classes.add(hashMap);
+        }
+        return classes;
     }
 
     @ApiResponses({
@@ -31,7 +54,7 @@ public class ApiResponseController {
             @ApiResponse(code = 204, message = "无数据")
     })
     @PostMapping("/test2")
-    public String test2(@RequestParam("name")String name, @RequestBody Address address) {
+    public String test2(@RequestParam("name") String name, @RequestBody Address address) {
 
         return name + address.toString();
     }
