bc28e38b5a4b6227a1c89ba7d6ccc0ffc5a9d92c
Levin <1837307557@qq.com>
2018-12-27 11:34:25 +0800

    添加测试案例

--- swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/entity/Message.java
+++ swagger-spring-boot-samples/swagger-spring-boot-sample-basic/src/main/java/com/swagger/entity/Message.java
@@ -1,10 +1,12 @@
 package com.swagger.entity;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel
 public class Message {
 
+    @ApiModelProperty(name = "消息", required = true)
     private String msg;
 
     public String getMsg() {
